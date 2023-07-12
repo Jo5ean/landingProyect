5f1668c5afb57436304b308ab9d1da105840b1c6
Franco <fran.lizarraga.23@gmail.com>
2023-07-12 11:45:53 -0300

    Fixing styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		fontFamily: {
-			sans: ['Museo Sans', 'sans-serif']
-		},
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["Museo Sans", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        "greya": "#5D6473",
+      },
+    },
+  },
+  plugins: [],
+};
