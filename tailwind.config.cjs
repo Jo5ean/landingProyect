4a51777fa9159507dec7c8cb225e321881176614
Jo5ean <josean.urbani@gmail.com>
2023-07-28 16:02:22 -0300

    Almost ready!

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     fontFamily: {
       sans: ["Museo Sans", "sans-serif"],
     },
     extend: {
       colors: {
-        "greya": "#5D6473",
+        greya: "#5D6473",
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
