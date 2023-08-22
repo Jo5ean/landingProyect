1239bd4bd5cce13215c9dc6dadd36d480705958c
Jo5ean <josean.urbani@gmail.com>
2023-08-22 17:01:22 -0300

    logicas y mas logicas

---
+++ src/scripts/facultades.js
@@ -0,0 +1,56 @@
+const facus =[
+    {
+      "nombre": "FACULTAD DE INGENIERIA",
+      "codare": 14,
+    },
+    {
+      "nombre": "FACULTAD DE ARTES Y CIENCIAS",
+      "codare": 11,
+    },
+    {
+      "nombre": "FACULTAD DE ARQUITECTURA Y URBANISMO",
+      "codare": 15,
+    },
+    {
+      "nombre": "FACULTAD DE CIENCIAS AGRARIAS Y VETERINARIAS",
+      "codare": 19,
+    },
+    {
+      "nombre": "FACULTAD DE CIENCIAS JURIDICAS",
+      "codare": 13,
+    },
+    {
+      "nombre": "FACULTAD DE ECONOMIA Y ADMINISTRACION",
+      "codare": 12,
+    },
+    {
+      "nombre": "FACULTAD DE EDUCACION",
+      "codare": 20,
+    },
+    {
+      "nombre": "FACULTAD DE EDUCACION FISICA",
+      "codare": 17,
+    },
+    {
+      "nombre": "FACULTAD DE MUSICA",
+      "codare": 21,
+    },
+    {
+      "nombre": "FACULTAD DE TEOLOGIA Y FILOSOFIA",
+      "codare": 111,
+    },
+    {
+      "nombre": "FACULTAD DE TURISMO",
+      "codare": 18,
+    },
+    {
+      "nombre": "FACULTAD DE CIENCIAS DE LA SALUD",
+      "codare": 24,
+    },
+    {
+      "nombre": "FACULTAD DE TRABAJO SOCIAL",
+      "codare": 16,
+    }
+  ]
+
+  export {facus}
